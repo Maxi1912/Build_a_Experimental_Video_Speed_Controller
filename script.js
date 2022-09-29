b491f8a961e2d571aa5884c37022384aab764416
Maxi1912 <maximus.sheva@gmail.com>
2022-09-29 17:41:51 +0300

    this is only begining

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const speed = document.querySelector('.speed');
 const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
+
+speed.addEventListener('click', function(e) => {
+  const y = e.
+})
