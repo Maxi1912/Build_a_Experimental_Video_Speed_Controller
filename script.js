5ccf3542744618c507b7e130312de1aecf739489
Maxi1912 <maximus.sheva@gmail.com>
2022-09-30 15:54:21 +0300

    done

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const speed = document.querySelector('.speed');
 const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
+console.log(bar);
 
-speed.addEventListener('click', function(e) => {
-  const y = e.
-})
+function handleMove(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  const playbackRate = percent * (max - min) + min;
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + '×';
+  console.log(bar);
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', handleMove);
